b8a8ecf36be9c326a94b604faa65b43c45aade0a
VuongNhatHien <hienvuongnhat@gmail.com>
2025-05-20 04:35:05 +0700

    Put backend url to env

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const data = await fetch("http://localhost:8080/test");
+  const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/test`);
   const string = await data.json();
   return <div>{string.data}</div>;
 }
