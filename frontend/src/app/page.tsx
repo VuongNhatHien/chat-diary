97ef984f6d8fa280918f7ee171432d88e121de4d
VuongNhatHien <hienvuongnhat@gmail.com>
2025-05-25 14:40:54 +0700

    Add docker compose for whole app

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
+// export const dynamic = 'force-dynamic';
+
 export default async function Home() {
-  //test2
-  const data = await fetch(`${process.env.BACKEND_URL}/test`);
+  const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/test`, {cache: 'no-store'});
   const string = await data.json();
   return <div>{string.data}</div>;
 }
